ec17409e0e0c40d54ee53c796c9a2243add49eb6
Fabien Roy <exilednarwal28@gmail.com>
2017-03-23 10:58:36 -0400

    Built New and Edit EmployeeActivity

--- app/src/main/java/net/info420/fabien/androidtravailpratique/EditEmployeeActivity.java
+++ app/src/main/java/net/info420/fabien/androidtravailpratique/EditEmployeeActivity.java
@@ -2,13 +2,28 @@ package net.info420.fabien.androidtravailpratique;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 public class EditEmployeeActivity extends AppCompatActivity {
   private final static String TAG = EditEmployeeActivity.class.getName();
+
+  private EditText etEmployeeName;
+  private EditText etEmployeeJob;
+  private EditText etEmployeeMail;
+  private EditText etEmployeePhone;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_edit_employee);
+
+    initUI();
+  }
+
+  private void initUI() {
+    etEmployeeName  = (EditText) findViewById(R.id.et_employee_name);
+    etEmployeeJob   = (EditText) findViewById(R.id.et_employee_job);
+    etEmployeeMail  = (EditText) findViewById(R.id.et_employee_mail);
+    etEmployeePhone = (EditText) findViewById(R.id.et_employee_phone);
   }
 }
--- app/src/main/java/net/info420/fabien/androidtravailpratique/NewEmployeeActivity.java
+++ app/src/main/java/net/info420/fabien/androidtravailpratique/NewEmployeeActivity.java
@@ -2,13 +2,28 @@ package net.info420.fabien.androidtravailpratique;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 public class NewEmployeeActivity extends AppCompatActivity {
   private final static String TAG = NewEmployeeActivity.class.getName();
+
+  private EditText etEmployeeName;
+  private EditText etEmployeeJob;
+  private EditText etEmployeeMail;
+  private EditText etEmployeePhone;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_new_employee);
+
+    initUI();
+  }
+
+  private void initUI() {
+    etEmployeeName  = (EditText) findViewById(R.id.et_employee_name);
+    etEmployeeJob   = (EditText) findViewById(R.id.et_employee_job);
+    etEmployeeMail  = (EditText) findViewById(R.id.et_employee_mail);
+    etEmployeePhone = (EditText) findViewById(R.id.et_employee_phone);
   }
 }
