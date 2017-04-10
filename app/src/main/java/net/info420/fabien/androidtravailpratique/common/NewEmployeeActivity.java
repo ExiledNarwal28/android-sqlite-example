4df0b6ae89a0e7514e127a8709574223bbdcafd5
Fabien Roy <exilednarwal28@gmail.com>
2017-04-10 11:55:22 -0400

    NewEmployeeActivity done

--- app/src/main/java/net/info420/fabien/androidtravailpratique/common/NewEmployeeActivity.java
+++ app/src/main/java/net/info420/fabien/androidtravailpratique/common/NewEmployeeActivity.java
@@ -1,27 +1,36 @@
 package net.info420.fabien.androidtravailpratique.common;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.telephony.PhoneNumberUtils;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 import net.info420.fabien.androidtravailpratique.R;
+import net.info420.fabien.androidtravailpratique.contentprovider.TaskerContentProvider;
+import net.info420.fabien.androidtravailpratique.utils.Employee;
 
 public class NewEmployeeActivity extends FragmentActivity {
   private final static String TAG = NewEmployeeActivity.class.getName();
 
-  private EditText etEmployeeName;
-  private EditText etEmployeeJob;
-  private EditText etEmployeeMail;
-  private EditText etEmployeePhone;
+  private EditText  etEmployeeName;
+  private EditText  etEmployeeJob;
+  private EditText  etEmployeeMail;
+  private EditText  etEmployeePhone;
+  private Button    btnValidate;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-
     setContentView(R.layout.activity_new_employee);
 
     initUI();
@@ -39,8 +48,61 @@ public class NewEmployeeActivity extends FragmentActivity {
     etEmployeeJob   = (EditText) findViewById(R.id.et_employee_job);
     etEmployeeMail  = (EditText) findViewById(R.id.et_employee_mail);
     etEmployeePhone = (EditText) findViewById(R.id.et_employee_phone);
+    btnValidate     = (Button)   findViewById(R.id.btn_validate);
+
+    btnValidate.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        createEmployee();
+      }
+    });
   }
 
+  public void createEmployee() {
+    String name   = etEmployeeName.getText().toString();
+    String job    = etEmployeeJob.getText().toString();
+    String mail   = etEmployeeMail.getText().toString();
+    String phone  = etEmployeePhone.getText().toString();
+
+    Log.d(TAG, String.format("Validation name   : length  : %s", name.length() == 0));
+    Log.d(TAG, String.format("Validation phone  : length  : %s", phone.length() != 0));
+    Log.d(TAG, String.format("Validation phone  : format  : %s", !PhoneNumberUtils.isGlobalPhoneNumber(phone)));
+    Log.d(TAG, String.format("Validation phone  : double  : %s", (phone.length() != 0 && !PhoneNumberUtils.isGlobalPhoneNumber(phone))));
+    Log.d(TAG, String.format("Validation mail   : length  : %s", mail.length() != 0 ));
+    Log.d(TAG, String.format("Validation mail   : format  : %s", !Patterns.EMAIL_ADDRESS.matcher(mail).matches()));
+    Log.d(TAG, String.format("Validation mail   : double  : %s", (mail.length() != 0 && !Patterns.EMAIL_ADDRESS.matcher(mail).matches())));
+
+    // Toutes les informations obligatoires doivent êtes présentes
+    if (name.length() == 0) {
+      Toast.makeText(getApplicationContext(), getString(R.string.warning_missing_required_fields), Toast.LENGTH_LONG).show();
+      return;
+    }
+
+    // Validation du numéro de téléphone
+    // Source : http://stackoverflow.com/questions/6358380/phone-number-validation-android#6359128
+    if (phone.length() != 0 && !PhoneNumberUtils.isGlobalPhoneNumber(phone)) {
+      Toast.makeText(getApplicationContext(), getString(R.string.warning_wrong_phone_format), Toast.LENGTH_LONG).show();
+      return;
+    }
+
+    // Validation de l'adresse e-mail
+    // Source : http://stackoverflow.com/questions/12947620/email-address-validation-in-android-on-edittext
+    if (mail.length() != 0 && !Patterns.EMAIL_ADDRESS.matcher(mail).matches()) {
+      Toast.makeText(getApplicationContext(), getString(R.string.warning_wrong_mail_format), Toast.LENGTH_LONG).show();
+      return;
+    }
+
+    ContentValues values = new ContentValues();
+    values.put(Employee.KEY_name,   name);
+    values.put(Employee.KEY_job,    job);
+    values.put(Employee.KEY_phone,  phone);
+    values.put(Employee.KEY_email,  mail);
+
+    // Nouvelle tâche
+    getContentResolver().insert(TaskerContentProvider.CONTENT_URI_EMPLOYEE, values);
+
+    finish();
+  }
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
